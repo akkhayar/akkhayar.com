0164271f429f244a838a63477dd9b6757b2c3610
Rickaym <psmtitant@gmail.com>
2023-11-25 21:56:15 +0630

    feat: update main page with new components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,30 +1,20 @@
-import { Fahkwang } from 'next/font/google'
-import { DM_Sans } from 'next/font/google'
+import { Fahkwang, DM_Sans } from 'next/font/google';
+import Footer from '@/components/Footer';
+import Navbar from '@/components/Navbar';
 
-
-const fahkwang = Fahkwang({ weight: "200", subsets: ['latin'] });
-
-const dm_sans = DM_Sans({ weight: "200", subsets: ['latin'] });
-
-function Navbar() {
-  return (
-    <nav className="flex justify-between items-center">
-      <div className="flex items-center">
-        <a href="#" className="mr-4">ABOUT</a>
-        <a href="#" className="mr-4">DOCS</a>
-        <a href="#">OPERATIONS</a>
-      </div>
-    </nav>
-  )
-}
+const fahkwang = Fahkwang({ weight: "700", subsets: ['latin'] });
+const dm_sans = DM_Sans({ weight: "400", subsets: ['latin'] });
 
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main>
       <Navbar />
-      <h1 className={fahkwang.className}>akkhayar</h1>
-      <h2 className={dm_sans.className}>Where potential meets reality.</h2>
+      <section className='py-8 flex flex-col items-center'>
+        <h1 className={fahkwang.className + ' text-9xl hover:text-black hover:bg-white'}>akkhayar</h1>
+        <h2 className={dm_sans.className + ' mt-3 text-2xl hover:text-black hover:bg-white'}>Where potential meets reality.</h2>
+      </section>
+      <Footer />
     </main>
   )
 }
