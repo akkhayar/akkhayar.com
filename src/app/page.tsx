a657581a142f51593173a4931b2d249f3045fedb
Rickaym <psmtitant@gmail.com>
2023-11-26 11:39:48 +0630

    feat: button styling and circle behind title text

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,10 @@
+"use client";
+
 import { Fahkwang, DM_Sans } from 'next/font/google';
 import Footer from '@/components/Footer';
 import Navbar from '@/components/Navbar';
+
+import { ReactLenis } from '@studio-freight/react-lenis'
 
 const fahkwang = Fahkwang({ weight: "700", subsets: ['latin'] });
 const dm_sans = DM_Sans({ weight: "400", subsets: ['latin'] });
@@ -8,13 +12,18 @@ const dm_sans = DM_Sans({ weight: "400", subsets: ['latin'] });
 
 export default function Home() {
   return (
-    <main>
-      <Navbar />
-      <section className='py-8 flex flex-col items-center'>
-        <h1 className={fahkwang.className + ' text-9xl hover:text-black hover:bg-white'}>akkhayar</h1>
-        <h2 className={dm_sans.className + ' mt-3 text-2xl hover:text-black hover:bg-white'}>Where potential meets reality.</h2>
-      </section>
-      <Footer />
-    </main>
+    <ReactLenis root>
+      <main className='mx-12'>
+        <Navbar />
+        <section className='static py-8 flex flex-col items-center' style={{marginBottom: '200px'}}>
+          <div className='absolute left-80 w-60 h-60 rounded-full bg-white '></div>
+          <h1 className={fahkwang.className + ' text-9xl title'}>akkhayar</h1>
+          <h2 className={dm_sans.className + ' mt-3 text-2xl'}>Where potential meets reality.</h2>
+        </section>
+        <div className={dm_sans.className + ' primary-btn'}>Description</div>
+        <div className='elongated'></div>
+        <Footer />
+      </main>
+    </ReactLenis>
   )
 }
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -6,19 +6,19 @@ const dm_sans = DM_Sans({ weight: "300", subsets: ['latin'] });
 
 function NavbarItem(props: { text: string }) {
   return (
-    <a href='#' className={dm_sans.className + ' mr-10 uppercase text-2xl hover:text-black hover:bg-white'}>{props.text}</a>
+    <a href='#' className={dm_sans.className + ' mr-10 uppercase text-2xl'}>{props.text}</a>
   )
 }
 
 export default function Navbar() {
   return (
-    <nav className='m-10 flex justify-between items-center'>
+    <nav className='mt-5 flex justify-between items-center'>
       <Image src='/logo.png' width={80} height={80} alt='akkhayar-logo'/>
       <div className='flex items-center'>
         <NavbarItem text='About' />
         <NavbarItem text='Docs' />
         <NavbarItem text='Operations' />
-        <div className={dm_sans.className + ' text-2xl uppercase spotlight-navbar-item hover:text-black hover:bg-white'}>What we are working on</div>
+        <div className={dm_sans.className + ' text-2xl uppercase spotlight-navbar-item'}>What we are working on</div>
       </div>
     </nav>
   )
